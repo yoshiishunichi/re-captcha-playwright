de43836cea7bb86dc339649166251169cf8c8a93
吉井駿一 <syun_syun01@icloud.com>
2022-11-17 15:57:02 +0900

    :sparkles: 一通りの動作を作成

---
+++ src/data/index.ts
@@ -0,0 +1,1 @@
+export const pageUrl = "https://re-captcha-bot.vercel.app/";
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,23 @@
 import { chromium } from "playwright";
+import { pageUrl } from "./data";
 
 (async () => {
-  const browser = await chromium.launch();
+  const browser = await chromium.launch({ headless: false });
   const page = await browser.newPage();
-  await page.goto("http://whatsmyuseragent.org/");
-  await page.screenshot({ path: `images/example.png` });
-  await browser.close();
+  await page.goto(pageUrl, {
+    waitUntil: "networkidle",
+  });
+  const reCaptchaContainer = page.locator("#re-captcha-container");
+  const coordinate = await reCaptchaContainer.boundingBox();
+  if (coordinate) {
+    const { x, y } = coordinate;
+
+    await page.waitForTimeout(1000);
+    await page.mouse.move(x, y);
+    await page.waitForTimeout(1000);
+    await page.mouse.click(x, y);
+    await page.waitForTimeout(1000);
+    await page.getByText("送信").click();
+  }
+  // await browser.close();
 })();
